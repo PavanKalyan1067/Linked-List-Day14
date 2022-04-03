217d56c6115a3f48fec47bea576dcc7f138388cd
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-03 23:40:38 +0530

    Creating A linked list

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace LinkedListDay14
+namespace LinkedListOperation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LinkedList!");
+            //UseCase 1
+            Operations obj = new Operations();
+            //Insertion Operation
+            obj.InsertLast(56);
+            obj.InsertLast(30);
+            obj.InsertLast(70);
+            //Display Operation
+            Console.WriteLine("Insertion at Last");
+            obj.Display();
         }
     }
 }
