c27c6cbb226a5611fcbd991c6087c26429c414cc
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-03 23:49:45 +0530

    Inserting in middle

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace LinkedListOperation
             obj.InsertFront(30);
             obj.InsertFront(70);
             //Display Operation
-            Console.WriteLine("Appending");
+            Console.WriteLine("Inserting In Middle");
             obj.Display();
         }
     }
