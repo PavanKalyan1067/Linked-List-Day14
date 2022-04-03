cf14f8b3fcc501a653df058cae254ff9550cfbd4
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-03 23:35:53 +0530

    Welcome to LinkedList

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDay14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList!");
+        }
+    }
+}
